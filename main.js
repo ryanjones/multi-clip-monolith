4fab35500c68657e8b85659a0cc86f09855039d0
Ryan Jones <ryan.michael.jones@gmail.com>
2017-11-13 20:28:20 -0700

    Fix windows Issues
    Specifically the DPI sizing. Also fix robotjs to paste properly on windows. I'm suprised there isn't a 'commandorcontrol'.

--- main.js
+++ main.js
@@ -8,19 +8,32 @@ let store = new Store();
 const MultiClipboard = require('./scripts/multi-clipboard-server.js').MultiClipboard;
 let multiClipboard = new MultiClipboard();
 
+
+if (process.platform === 'win32') {
+  app.commandLine.appendSwitch('high-dpi-support', 1)
+  app.commandLine.appendSwitch('force-device-scale-factor', 1)
+}
+
 const path = require('path');
 const url = require('url');
 
 let mainWindow;
 
 function createWindow() {
-  mainWindow = new BrowserWindow({width: 220,
-    height: 240,
-    show: false,
-    frame: false});
+  let windowSize = {}
+  if (process.platform === 'win32') {
+    windowSize = {width: 340, height: 360, show: false, frame: false}
+  }
+  else {
+    windowSize = {width: 220, height: 240, show: false, frame: false}    
+  }
+
+
+  mainWindow = new BrowserWindow(windowSize);
 
   mainWindow.webContents.on('dom-ready', function() {
     console.log('dom-ready');
+    console.log('work area size, X:' + electron.screen.getPrimaryDisplay().workAreaSize.width + ' Y:' + electron.screen.getPrimaryDisplay().workAreaSize.height)
     refreshClipBoard();
   });
   
@@ -81,15 +94,30 @@ let ipcMain = require('electron').ipcMain;
 ipcMain.on('paste', (event, arg) => {
   if (process.platform === 'darwin') {
     Menu.sendActionToFirstResponder('hide:');
+
+    setTimeout(function() {
+      console.log('down')
+      robot.keyToggle('v', 'down', ['command']);
+      setTimeout(function() {
+        console.log('up')
+        robot.keyToggle('v', 'up', ['command']);
+      }, 100);
+    }, 100);
+    mainWindow.hide();    
   }
 
-  setTimeout(function() {
-    robot.keyToggle('v', 'down', ['command']);
+  if (process.platform === 'win32') {
+    mainWindow.minimize();  
     setTimeout(function() {
-      robot.keyToggle('v', 'up', ['command']);
+      console.log('down')
+      robot.keyToggle('v', 'down', ['control']);
+      setTimeout(function() {
+        console.log('up')
+        robot.keyToggle('v', 'up', ['control']);
+      }, 100);
     }, 100);
-  }, 100);
-  mainWindow.hide();
+    mainWindow.hide();
+  }
 });
 
 
